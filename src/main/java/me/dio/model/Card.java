8e00114ea74443eb5ca12f63f93e90c891738e71
JuliaLouback <julialoubackribeiro2001@gmail.com>
2024-07-29 20:49:01 -0300

    Camada de Dominio

---
+++ src/main/java/me/dio/model/Card.java
@@ -0,0 +1,49 @@
+package me.dio.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_card")
+public class Card {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(precision = 2, scale = 13, name = "additional_limit")
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+    
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
